b435fac16a5b41a9564de5e5017ab9da3b24db8e
bolortoli <bolortoli@csms.edu.mn>
2021-03-18 02:30:37 -0400

    Created strapi app

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '39c8165f171f837b10309760fe573914'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '74cbae2c-0d2f-42ed-aeab-4cd030284c22'
+};
