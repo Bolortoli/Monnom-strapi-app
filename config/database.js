ce3303ba87be4e3d52fc74fbd61de1bc48fcbe41
Battulga <battulga@Macbooks-MacBook-Pro-2.local>
2021-05-30 14:55:13 +0800

    migrate to postgres

--- config/database.js
+++ config/database.js
@@ -1,15 +1,39 @@
-module.exports = ({ env }) => ({
-  defaultConnection: 'default',
-  connections: {
-    default: {
-      connector: 'bookshelf',
-      settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
-      },
-      options: {
-        useNullAsDefault: true,
+const isPostgres = true;
+if (isPostgres){
+  module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'postgres',
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'monnom'),
+          username: env('DATABASE_USERNAME', 'monnom'),
+          password: env('DATABASE_PASSWORD', 'admin'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
       },
     },
-  },
-});
+  });
+}else {
+  module.exports = ({ env }) => ({
+    defaultConnection: 'default',
+    connections: {
+      default: {
+        connector: 'bookshelf',
+        settings: {
+          client: 'sqlite',
+          filename: env('DATABASE_FILENAME', '.tmp/data.sqlite3'),
+        },
+        options: {
+          useNullAsDefault: true,
+        },
+      },
+    },
+  });
+}
+
